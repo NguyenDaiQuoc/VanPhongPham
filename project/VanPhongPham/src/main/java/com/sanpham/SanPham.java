6e04973a01595c1a0c5719f48f04073c88fcbfe2
Sang0912 <152009046+Sang0912@users.noreply.github.com>
2023-11-29 15:32:20 +0700

    Add files via upload

--- project/VanPhongPham/src/main/java/com/sanpham/SanPham.java
+++ project/VanPhongPham/src/main/java/com/sanpham/SanPham.java
@@ -2,84 +2,20 @@
 package com.mycompany.first.DoAn;
 
 
-import java.util.Scanner;
-
-
-public abstract class SanPham {
-    public String idSanPham;
-    public String name;
-    public float gia;
-    public int soluong;
-    public String ngaySx;
-    public String donviSx;
-    public String loai;
+public interface SanPham {
+    public abstract String getIdSanpham();
     
-    Scanner sc = new Scanner(System.in);
-
-    public SanPham() {
-    }
-
-    public SanPham(String idSanPham, String name, float gia, int soluong, String ngaySx, String donviSx, String loai) {
-        this.idSanPham = idSanPham;
-        this.name = name;
-        this.gia = gia;
-        this.soluong = soluong;
-        this.ngaySx = ngaySx;
-        this.donviSx = donviSx;
-        this.loai = loai;
-    }
-
-    public String getIdSanPham() {
-        return idSanPham;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public float getGia() {
-        return gia;
-    }
-
-    public int getSoluong() {
-        return soluong;
-    }
-
-    public String getNgaySx() {
-        return ngaySx;
-    }
-
-    public String getDonviSx() {
-        return donviSx;
-    }
-
-    public abstract String getLoai();
-
-    public void setIdSanPham(String idSanPham) {
-        this.idSanPham = idSanPham;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setGia(float gia) {
-        this.gia = gia;
-    }
-
-    public void setSoluong(int soluong) {
-        this.soluong = soluong;
-    }
-
-    public void setNgaySx(String ngaySx) {
-        this.ngaySx = ngaySx;
-    }
-
-    public void setDonviSx(String donviSx) {
-        this.donviSx = donviSx;
-    }
-
-    public abstract void setLoai(String loai);
+    public abstract String getName();
+    
+    public abstract float getGia();
+    
+    public abstract int getSoluong();
+    
+    public abstract String getNgaySx();
+    
+    public abstract String getDonviSx();
+    
+    public abstract void setIdSanpham();
     
     public abstract void nhapSanpham();
     
