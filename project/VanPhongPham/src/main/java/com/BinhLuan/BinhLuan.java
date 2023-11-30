d41abbba4eae7b1d8d718ed42b9f818256bf6bec
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-30 13:05:52 +0700

    Update BinhLuan.java
    - Đã thêm get/set/constructor còn thiếu

--- project/VanPhongPham/src/main/java/com/BinhLuan/BinhLuan.java
+++ project/VanPhongPham/src/main/java/com/BinhLuan/BinhLuan.java
@@ -3,16 +3,69 @@ class BinhLuan {
     private String noiDung;
     private Date ngayDang;
     private SanPham sanPham;
+    private String maKhachHang;
 
-    public BinhLuan(String maBinhLuan, String noiDung, SanPham sanPham) {
+    // Default constructor
+    public BinhLuan() {
+    }
+
+    // Constructor with all fields
+    public BinhLuan(String maBinhLuan, String noiDung, Date ngayDang, SanPham sanPham, String maKhachHang) {
         this.maBinhLuan = maBinhLuan;
         this.noiDung = noiDung;
-        this.ngayDang = new Date();
+        this.ngayDang = ngayDang;
         this.sanPham = sanPham;
+        this.maKhachHang = maKhachHang;
+    }
+
+    // Existing constructor
+    public BinhLuan(String maBinhLuan, String noiDung, SanPham sanPham, String maKhachHang) {
+        this(maBinhLuan, noiDung, new Date(), sanPham, maKhachHang);
+    }
+
+    // Getter and Setter methods for maBinhLuan
+    public String getMaBinhLuan() {
+        return maBinhLuan;
+    }
+
+    public void setMaBinhLuan(String maBinhLuan) {
+        this.maBinhLuan = maBinhLuan;
+    }
+
+    // Getter and Setter methods for noiDung
+    public String getNoiDung() {
+        return noiDung;
     }
 
     public void setNoiDung(String noiDung) {
         this.noiDung = noiDung;
+    }
+
+    // Getter and Setter methods for ngayDang
+    public Date getNgayDang() {
+        return ngayDang;
+    }
+
+    public void setNgayDang(Date ngayDang) {
+        this.ngayDang = ngayDang;
+    }
+
+    // Getter and Setter methods for sanPham
+    public SanPham getSanPham() {
+        return sanPham;
+    }
+
+    public void setSanPham(SanPham sanPham) {
+        this.sanPham = sanPham;
+    }
+
+    // Getter and Setter methods for maKhachHang
+    public String getMaKhachHang() {
+        return maKhachHang;
+    }
+
+    public void setMaKhachHang(String maKhachHang) {
+        this.maKhachHang = maKhachHang;
     }
 
     public void luuBinhLuan() {
@@ -31,7 +84,7 @@ class BinhLuan {
             FileWriter writer = new FileWriter(file, true);
             SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
             String strDate = formatter.format(ngayDang);  
-            writer.write(maBinhLuan + ", " + noiDung + ", " + strDate + ", " + sanPham.getTen() + "\n");
+            writer.write(maBinhLuan + ", " + noiDung + ", " + strDate + ", " + sanPham.getTen() + ", " + maKhachHang + "\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Co loi xay ra khi luu binh luan.");
