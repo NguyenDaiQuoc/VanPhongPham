e0979eb2cec31cf2da1d458f18e927238e8ed038
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-30 21:28:19 +0700

    Update KhachHang.java

--- project/VanPhongPham/src/main/java/com/khachhang/KhachHang.java
+++ project/VanPhongPham/src/main/java/com/khachhang/KhachHang.java
@@ -12,84 +12,19 @@ import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.Calendar;
 
-public class KhachHang extends NguoiDung {
-    private String maNguoiDung;
-    private String tenDangNhap;
-    private String hoTen;
-    private String soDienThoai;
-    private String diaChi;
-
-    // Default constructor
-    public KhachHang() {
+class KhachHang extends NguoiDung {
+    KhachHang(String tenDangNhap, String matKhau) {
+        super(sTenDangNhap, sMatKhau, "KhachHang");
     }
 
-    // Parameterized constructor
-    public KhachHang(String maNguoiDung, String tenDangNhap, String hoTen, String soDienThoai, String diaChi) {
-        this.maNguoiDung = maNguoiDung;
-        this.tenDangNhap = tenDangNhap;
-        this.hoTen = hoTen;
-        this.soDienThoai = soDienThoai;
-        this.diaChi = diaChi;
-    }
-
-    // Copy constructor
-    public KhachHang(KhachHang other) {
-        this.maNguoiDung = other.maNguoiDung;
-        this.tenDangNhap = other.tenDangNhap;
-        this.hoTen = other.hoTen;
-        this.soDienThoai = other.soDienThoai;
-        this.diaChi = other.diaChi;
-    }
-
-    // Getters
-    public String getMaNguoiDung() {
-        return maNguoiDung;
-    }
-
-    public String getTenDangNhap() {
-        return tenDangNhap;
-    }
-
-    public String getHoTen() {
-        return hoTen;
-    }
-
-    public String getSoDienThoai() {
-        return soDienThoai;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    // Setters
-    public void setMaNguoiDung(String maNguoiDung) {
-        this.maNguoiDung = maNguoiDung;
-    }
-
-    public void setTenDangNhap(String tenDangNhap) {
-        this.tenDangNhap = tenDangNhap;
-    }
-
-    public void setHoTen(String hoTen) {
-        this.hoTen = hoTen;
-    }
-
-    public void setSoDienThoai(String soDienThoai) {
-        this.soDienThoai = soDienThoai;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
     @Override
     void nhapThongTin(Scanner scanner) {
         System.out.print("Nhap ho ten: ");
-        this.hoTen = scanner.nextLine();
+        this.sHoTen = scanner.nextLine();
         System.out.print("Nhap so dien thoai: ");
-        this.soDienThoai = scanner.nextLine();
+        this.sSoDienThoai = scanner.nextLine();
         System.out.print("Nhap dia chi: ");
-        this.diaChi = scanner.nextLine();
+        this.sDiaChi = scanner.nextLine();
     }
 }
 
