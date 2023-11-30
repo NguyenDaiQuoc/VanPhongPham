4b4f27edeaddce27155a5d134aaebf507de931af
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-11-30 13:20:29 +0700

    Update PhuongThucThanhToan.java
    - Đã thêm get set constructor

--- project/VanPhongPham/src/main/java/com/PhuongThucThanhToan/PhuongThucThanhToan.java
+++ project/VanPhongPham/src/main/java/com/PhuongThucThanhToan/PhuongThucThanhToan.java
@@ -1,32 +1,49 @@
-class PhuongThucThanhToan {
+public class PhuongThucThanhToan {
     private String maPhuongThuc;
     private String ten;
     private String mota;
 
+    // Default constructor
+    public PhuongThucThanhToan() {
+        this.maPhuongThuc = "";
+        this.ten = "";
+        this.mota = "";
+    }
+
+    // Parameterized constructor
     public PhuongThucThanhToan(String maPhuongThuc, String ten, String mota) {
         this.maPhuongThuc = maPhuongThuc;
         this.ten = ten;
         this.mota = mota;
     }
 
-    public String getMaPhuongThuc() {
-        return maPhuongThuc;
+    // Copy constructor
+    public PhuongThucThanhToan(PhuongThucThanhToan other) {
+        this.maPhuongThuc = other.maPhuongThuc;
+        this.ten = other.ten;
+        this.mota = other.mota;
     }
 
-    public void setMaPhuongThuc(String maPhuongThuc) {
-        this.maPhuongThuc = maPhuongThuc;
+    // Getters
+    public String getMaPhuongThuc() {
+        return maPhuongThuc;
     }
 
     public String getTen() {
         return ten;
     }
 
-    public void setTen(String ten) {
-        this.ten = ten;
-    }
-
     public String getMota() {
         return mota;
+    }
+
+    // Setters
+    public void setMaPhuongThuc(String maPhuongThuc) {
+        this.maPhuongThuc = maPhuongThuc;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
     }
 
     public void setMota(String mota) {
