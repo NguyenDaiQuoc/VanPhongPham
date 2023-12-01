eded89b9c01fa4b12fae71f8547633b32d90e55f
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-12-01 15:20:23 +0700

    Update DonHang.java

--- project/VanPhongPham/src/main/java/com/DonHang/DonHang.java
+++ project/VanPhongPham/src/main/java/com/DonHang/DonHang.java
@@ -1,9 +1,10 @@
 package com.DonHang;
 
-import com.KhachHang;
-import com.PhuongThucThanhToan;
-import com.GioHang;
-import com.SanPham;
+import com.KhachHang.KhachHang;
+import com.GioHang.GioHang;
+import com.PhuongThucThanhToan.PhuongThucThanhToan;
+import com.SanPham.SanPham;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -17,11 +18,13 @@ public class DonHang {
     private String trangThaiDonHang;
     private PhuongThucThanhToan phuongThucThanhToan;
 
+    // Default constructor
     public DonHang() {
         this.trangThaiDonHang = "Chua xu ly";
         this.ngayDatHang = new Date();
     }
 
+    // Parameterized constructor
     public DonHang(String maDonHang, KhachHang khachHang, GioHang gioHang, PhuongThucThanhToan phuongThucThanhToan) {
         this.maDonHang = maDonHang;
         this.khachHang = khachHang;
@@ -34,13 +37,13 @@ public class DonHang {
     // Copy constructor
     public DonHang(DonHang other) {
         this.maDonHang = other.maDonHang;
-        this.khachHang = other.khachHang;  // This assumes that KhachHang is immutable or that it's okay to share references
-        this.gioHang = other.gioHang;  // This assumes that GioHang is immutable or that it's okay to share references
+        this.khachHang = new KhachHang(other.khachHang); // Create a new KhachHang instance to avoid sharing references
+        this.gioHang = new GioHang(other.gioHang); // Create a new GioHang instance to avoid sharing references
         this.ngayDatHang = other.ngayDatHang;
         this.trangThaiDonHang = other.trangThaiDonHang;
-        this.phuongThucThanhToan = other.phuongThucThanhToan;  // This assumes that PhuongThucThanhToan is immutable or that it's okay to share references
+        this.phuongThucThanhToan = other.phuongThucThanhToan; // This assumes that PhuongThucThanhToan is immutable or that it's okay to share references
     }
-    
+
     // Constructor with only maDonHang
     public DonHang(String maDonHang) {
         this.maDonHang = maDonHang;
@@ -51,32 +54,34 @@ public class DonHang {
     public void luuDonHang() {
         try {
             FileWriter writer = new FileWriter("Don Hang.txt", true);
-            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
-            String strDate = formatter.format(ngayDatHang);  
-            writer.write(khachHang.getMaNguoiDung() + ", " + maDonHang + ", " + khachHang.getHoTen() + ", " + strDate + ", " + trangThaiDonHang);
-    
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            String strDate = formatter.format(ngayDatHang);
+            writer.write(
+                    khachHang.getMaNguoiDung() + ", " + maDonHang + ", " + khachHang.getHoTen() + ", " + strDate + ", "
+                            + trangThaiDonHang);
+
             // Calculate the total amount
             int tongSoTien = 0;
             SanPham[] sanPhamTrongGio = gioHang.getGioHang();
             for (int i = 0; i < gioHang.getSoLuongSanPhamTrongGio(); i++) {
                 tongSoTien += sanPhamTrongGio[i].getGia();
             }
-    
+
             // Write the total amount and the payment method to the file
             writer.write(", " + tongSoTien + ", " + phuongThucThanhToan.getTen());
-    
+
             // Write the product information to the file
             for (int i = 0; i < gioHang.getSoLuongSanPhamTrongGio(); i++) {
                 writer.write(", " + sanPhamTrongGio[i].getName());
             }
-    
+
             writer.write("\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Có lỗi xảy ra khi lưu đơn hàng.");
             e.printStackTrace();
         }
-    }        
+    }
 
     public String getMaDonHang() {
         return maDonHang;
@@ -103,19 +108,19 @@ public class DonHang {
     }
 
     public void setMaDonHang(String maDonHang) {
-    this.maDonHang = maDonHang;
+        this.maDonHang = maDonHang;
     }
 
     public void setKhachHang(KhachHang khachHang) {
-    this.khachHang = khachHang;
+        this.khachHang = khachHang;
     }
 
     public void setGioHang(GioHang gioHang) {
-    this.gioHang = gioHang;
+        this.gioHang = gioHang;
     }
 
     public void setNgayDatHang(Date ngayDatHang) {
-    this.ngayDatHang = ngayDatHang;
+        this.ngayDatHang = ngayDatHang;
     }
 
     public PhuongThucThanhToan getPhuongThucThanhToan() {
