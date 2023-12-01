f33391f9416f85a74ab0f720acf091557f2cd46d
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-12-01 15:22:49 +0700

    Update KhachHang.java

--- project/VanPhongPham/src/main/java/com/khachhang/KhachHang.java
+++ project/VanPhongPham/src/main/java/com/khachhang/KhachHang.java
@@ -1,34 +1,50 @@
 package com.KhachHang;
 
-import com.nguoidung;
-import java.io.*;
-import java.util.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.util.Calendar;
+import com.NguoiDung.NguoiDung;
 
-class KhachHang extends NguoiDung {
+import java.util.Scanner;
+
+public class KhachHang extends NguoiDung {
     // Default constructor
-    KhachHang() {
+    public KhachHang() {
         super("", "", "KhachHang");
     }
 
-    KhachHang(String tenDangNhap, String matKhau) {
-        super(sTenDangNhap, sMatKhau, "KhachHang");
+    // Parameterized constructor
+    public KhachHang(String tenDangNhap, String matKhau) {
+        super(tenDangNhap, matKhau, "KhachHang");
+    }
+    
+    // Copy constructor
+    public KhachHang(KhachHang other) {
+        super(other.sTenDangNhap, other.sMatKhau, other.sVaiTro);
+        this.sHoTen = other.sHoTen;
+        this.sSoDienThoai = other.sSoDienThoai;
+        this.sDiaChi = other.sDiaChi;
     }
 
     @Override
-    void nhapThongTin(Scanner scanner) {
+    public void NhapThongTin() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Nhap ho ten: ");
         this.sHoTen = scanner.nextLine();
         System.out.print("Nhap so dien thoai: ");
         this.sSoDienThoai = scanner.nextLine();
         System.out.print("Nhap dia chi: ");
         this.sDiaChi = scanner.nextLine();
+    }
+
+    @Override
+    public void LayThongTin() {
+        System.out.println("Ho ten: " + sHoTen);
+        System.out.println("So dien thoai: " + sSoDienThoai);
+        System.out.println("Dia chi: " + sDiaChi);
+    }
+
+    @Override
+    public void LayTaiKhoan() {
+        System.out.println("Ten dang nhap: " + sTenDangNhap);
+        System.out.println("Mat khau: " + sMatKhau);
+        System.out.println("Vai tro: " + sVaiTro);
     }
 }
