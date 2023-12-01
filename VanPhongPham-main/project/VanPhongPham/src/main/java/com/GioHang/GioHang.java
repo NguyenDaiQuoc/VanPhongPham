d12eeb809c41966b3d8c4fd5ff3430ec10edc148
TuiLaZit <147925219+TuiLaZit@users.noreply.github.com>
2023-12-01 22:12:18 +0700

    update fix package
    <3

---
+++ VanPhongPham-main/project/VanPhongPham/src/main/java/com/GioHang/GioHang.java
@@ -0,0 +1,269 @@
+package com.GioHang;
+
+import com.PhuongThucThanhToan.PhuongThucThanhToan;
+import com.SanPham.SanPham;
+//import com.HienThiSanPham.HienThiSanPham;
+import com.DonHang.DonHang;
+import com.KhachHang.KhachHang;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class GioHang {
+    private SanPham[] gioHang = new SanPham[100]; // Assuming a maximum of 100 products in the cart
+    private int soLuongDonHang;
+    private int soLuongSanPhamTrongGio = 0;
+    // Define phuongThucThanhToans
+    private PhuongThucThanhToan[] phuongThucThanhToans;
+    private String maDonHang = generateMaDonHang();
+
+    // Default constructor
+    public GioHang() {
+        // Initialize phuongThucThanhToans in the constructor
+        phuongThucThanhToans = new PhuongThucThanhToan[3];
+        phuongThucThanhToans[0] = new PhuongThucThanhToan("1", "The tin dung", "Thanh toán bằng thẻ tín dụng");
+        phuongThucThanhToans[1] = new PhuongThucThanhToan("2", "Momo", "Thanh toán bằng ví điện tử Momo");
+        phuongThucThanhToans[2] = new PhuongThucThanhToan("3", "COD", "Thanh toán khi nhận hàng (COD)");
+    }
+
+    // Copy constructor
+    public GioHang(GioHang other) {
+        this.gioHang = other.gioHang.clone(); // This creates a new array that is a copy of the original
+        this.soLuongDonHang = other.soLuongDonHang;
+        this.soLuongSanPhamTrongGio = other.soLuongSanPhamTrongGio;
+        this.phuongThucThanhToans = other.phuongThucThanhToans.clone(); // This creates a new array that is a copy of the original
+        this.maDonHang = other.maDonHang;
+    }
+
+    // Parameterized constructor
+    public GioHang(SanPham[] gioHang, int soLuongDonHang, int soLuongSanPhamTrongGio, PhuongThucThanhToan[] phuongThucThanhToans, String maDonHang) {
+        this.gioHang = gioHang;
+        this.soLuongDonHang = soLuongDonHang;
+        this.soLuongSanPhamTrongGio = soLuongSanPhamTrongGio;
+        this.phuongThucThanhToans = phuongThucThanhToans;
+        this.maDonHang = maDonHang;
+    }
+
+    // Getters and Setters
+    public SanPham[] getGioHang() {
+        return gioHang;
+    }
+
+    public void setGioHang(SanPham[] gioHang) {
+        this.gioHang = gioHang;
+    }
+
+    public int getSoLuongDonHang() {
+        return soLuongDonHang;
+    }
+
+    public void setSoLuongDonHang(int soLuongDonHang) {
+        this.soLuongDonHang = soLuongDonHang;
+    }
+
+    public int getSoLuongSanPhamTrongGio() {
+        return soLuongSanPhamTrongGio;
+    }
+
+    public void setSoLuongSanPhamTrongGio(int soLuongSanPhamTrongGio) {
+        this.soLuongSanPhamTrongGio = soLuongSanPhamTrongGio;
+    }
+
+    public PhuongThucThanhToan[] getPhuongThucThanhToans() {
+        return phuongThucThanhToans;
+    }
+
+    public void setPhuongThucThanhToans(PhuongThucThanhToan[] phuongThucThanhToans) {
+        this.phuongThucThanhToans = phuongThucThanhToans;
+    }
+
+    public String getMaDonHang() {
+        return maDonHang;
+    }
+
+    public void setMaDonHang(String maDonHang) {
+        this.maDonHang = maDonHang;
+    }
+
+    // Other methods
+
+    void muaSanPham(SanPham[] sanPhamList, Scanner scanner) {
+        boolean displayMenu = true;
+
+        while (true) {
+            if (displayMenu) {
+                System.out.println("Chon loai san pham de mua:");
+                System.out.println("1. Sach");
+                System.out.println("2. Vo");
+                System.out.println("3. But");
+                System.out.println("11. Quay lai");
+                System.out.print("Nhap lua chon (1, 2, 3 hoac 11): ");
+            }
+
+            int productChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (productChoice >= 1 && productChoice <= 3) {
+                int startIndex = (productChoice - 1) * 10;
+                int endIndex = productChoice * 10 - 1;
+                //HienThiSanPham.hienThiSanPham(sanPhamList, startIndex, endIndex);
+
+                // User choice for buying the product
+                int buyChoice;
+                do {
+                    System.out.print("Nhap lua chon san pham (1 den 10) hoac 11 de quay lai: ");
+                    buyChoice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    if (buyChoice >= 1 && buyChoice <= 10) {
+                        if (soLuongSanPhamTrongGio < 100) { // Check if the cart is not full
+                            gioHang[soLuongSanPhamTrongGio] = sanPhamList[startIndex + buyChoice - 1];
+                            soLuongSanPhamTrongGio++;
+                            System.out.println("Da them san pham vao gio hang.");
+                        } else {
+                            System.out.println("Gio hang da day, khong the them san pham.");
+                            break; // Break out of the do-while loop
+                        }
+                    } else if (buyChoice == 11) {
+                        displayMenu = true; // Go back to the main menu
+                        return;
+                    } else {
+                        System.out.println("Lua chon khong hop le.");
+                    }
+                } while (true); // Continue until the user chooses to go back
+            } else if (productChoice == 11) {
+                break; // Quay lai trang truoc
+            } else {
+                System.out.println("Lua chon khong hop le.");
+            }
+
+            displayMenu = false;
+        }
+    }
+
+    private String generateMaDonHang() {
+        int lastOrderId = 0;
+        File file = new File("lastorderid.txt");
+
+        // Create the file if it doesn't exist
+        if (!file.exists()) {
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + file.getName());
+                } else {
+                    System.out.println("File already exists.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        // Read the last order ID from the file
+        try {
+            Scanner scanner = new Scanner(file);
+            if (scanner.hasNextInt()) {
+                lastOrderId = scanner.nextInt();
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find lastorderid.txt.");
+            e.printStackTrace();
+        }
+
+        lastOrderId++; // Increment the last order ID
+
+        // Write the new last order ID to the file
+        try {
+            FileWriter writer = new FileWriter("lastorderid.txt");
+            writer.write(String.valueOf(lastOrderId));
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to lastorderid.txt.");
+            e.printStackTrace();
+        }
+
+        return "DH" + lastOrderId; // Return the new order ID
+    }
+
+    public void docDonHang(KhachHang khachHang) {
+        try {
+            File file = new File("donhang.txt");
+            Scanner reader = new Scanner(file);
+
+            System.out.println("Cac don hang da dat:");
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                String[] orderInfo = data.split(", ");
+                // Check if the user ID matches
+                if (orderInfo[0].equals(khachHang.getMaNguoiDung())) {
+                    // Display the order information
+                    System.out.println("Ma Don Hang: " + orderInfo[1]);
+                    System.out.println("Khach Hang: " + orderInfo[2]);
+                    System.out.println("Ngay Dat Hang: " + orderInfo[3]);
+                    System.out.println("Trang Thai Don Hang: " + orderInfo[4]);
+                    System.out.println("Tong So Tien: " + orderInfo[5] + " VND");
+                    System.out.println("Phuong Thuc Thanh Toan: " + orderInfo[6]);
+
+                    // Display the product information
+                    for (int i = 7; i < orderInfo.length; i++) {
+                        System.out.println("San Pham: " + orderInfo[i]);
+                    }
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Khong tim thay tep don hang.");
+            e.printStackTrace();
+        }
+    }
+
+    DonHang[] donHangArray = new DonHang[100000];
+
+    void datDonHang(Scanner scanner, KhachHang khachHang) {
+        System.out.println("Chon phuong thuc thanh toan:");
+        for (PhuongThucThanhToan pttt : phuongThucThanhToans) {
+            System.out.println(pttt.getMaPhuongThuc() + ". " + pttt.getTen());
+        }
+        System.out.print("Nhap lua chon (1, 2 hoac 3): ");
+        int luaChon = scanner.nextInt();
+        scanner.nextLine(); // Consume newline left-over
+
+        PhuongThucThanhToan phuongThucThanhToan = phuongThucThanhToans[luaChon - 1]; // Get the chosen payment method
+        DonHang donHang = new DonHang(maDonHang, khachHang, this, phuongThucThanhToan);
+        switch (luaChon) {
+            case 1:
+                System.out.print("Nhap so the ");
+                String soThe = scanner.nextLine();
+                System.out.print("Nhap ma CVV ");
+                String maCVV = scanner.nextLine();
+                System.out.print("Nhap ngay het han (mm/yy): ");
+                String ngayHetHan = scanner.nextLine();
+                // Here you would usually send this information securely to your payment processor
+                // But for this example, we'll just print a success message
+                System.out.println("Da thanh toan thanh cong bang the tin dung!");
+                donHang.luuDonHang(); // Save the order to the file
+                break;
+            case 2:
+                System.out.println("Hay chuyen tien cho tai khoan nay: 0931816175, Ten: Dinh Phuc Thinh");
+                donHang.luuDonHang(); // Save the order to the file
+                break;
+            case 3:
+                System.out.println("Dat hang thanh cong! San pham se duoc thanh toan khi nhan hang");
+                donHang.luuDonHang(); // Save the order to the file
+                break;
+            default:
+                System.out.println("Lua chon khong hop le.");
+                break;
+        }
+    }
+
+    void xemGioHang() {
+        System.out.println("Danh sach san pham trong gio hang:");
+        for (int i = 0; i < soLuongSanPhamTrongGio; i++) {
+            System.out.println((i + 1) + ". " + gioHang[i].getName() + " - " + gioHang[i].getGia() + "VND");
+        }
+    }
+}
---
+++ VanPhongPham-main/project/VanPhongPham/src/main/java/com/QuanLy/QuanLyFile.java
@@ -0,0 +1,252 @@
+package com.QuanLy;
+
+import java.io.File;
+import java.io.IOException;
+import com.NguoiDung.NguoiDung;
+import com.SanPham.DanhSachBut;
+import com.SanPham.DanhSachSach;
+import com.SanPham.DanhSachVo;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import com.SanPham.SanPham;
+
+public class QuanLyFile extends NguoiDung {
+
+    Scanner sc = new Scanner(System.in);
+    String sTaiKhoanQuanLy, sMatKhauQuanLy;
+    String FileNameQuanLy = "Account Quan Ly.txt";
+    String FileThongTinQL = "Thong tin Quan Ly.txt";
+    String FileTmp = "tmp.txt";
+    String sHoTenQuanLy, sSoDienThoaiQuanLy, sDiaChiQuanLy;
+    SanPham[] danhSachSanPham = new SanPham[10];
+
+    //constructor
+    public QuanLyFile(String sTenDangNhap, String sMatKhau, String sVaiTro) {
+        super(sTenDangNhap, sMatKhau, "QuanLy");
+    }
+
+    public QuanLyFile() {
+    }
+
+    public QuanLyFile(String sTaiKhoanQuanLy, String sMatKhauQuanLy) {
+        this.sTaiKhoanQuanLy = sTaiKhoanQuanLy;
+        this.sMatKhauQuanLy = sMatKhauQuanLy;
+    }
+
+    public QuanLyFile(QuanLyFile a) {
+        this.sTaiKhoanQuanLy = a.sTaiKhoanQuanLy;
+        this.sMatKhauQuanLy = a.sMatKhauQuanLy;
+        this.sVaiTro = a.sVaiTro;
+        this.iTrangThai = a.iTrangThai;
+        this.sMaNguoiDung = GenerateUID();
+    }
+
+    //getter & setter 
+    public String getHoTen() {
+        return sHoTenQuanLy;
+    }
+
+    public void setHoTen(String sHoTen) {
+        for (;;) {
+            System.out.print("Moi nhap ho ten Quan Ly (khong duoc de trong) ");
+            sHoTenQuanLy = sc.nextLine();
+            this.sHoTenQuanLy = sHoTenQuanLy;
+            if (sHoTenQuanLy != "") {
+                break;
+            }
+        }
+    }
+
+    public String getSoDienThoai() {
+        return sSoDienThoaiQuanLy;
+    }
+
+    public void setSoDienThoai(String sSoDienThoai) {
+        for (;;) {
+            System.out.print("Moi so dien thoai Quan Ly ");
+            sSoDienThoaiQuanLy = sc.nextLine();
+            this.sSoDienThoaiQuanLy = sSoDienThoaiQuanLy;
+            if (sSoDienThoaiQuanLy != "") {
+                break;
+            }
+        }
+    }
+
+    public String getDiaChi() {
+        return sDiaChiQuanLy;
+    }
+
+    public void setDiaChi(String sDiaChi) {
+        for (;;) {
+            System.out.print("Moi nhap dia chi Quan Ly ");
+            sDiaChiQuanLy = sc.nextLine();
+            this.sDiaChiQuanLy = sDiaChiQuanLy;
+            if (sDiaChiQuanLy != "") {
+                break;
+            }
+        }
+    }
+
+    //Override de nhap thong tin ca nhan
+    @Override
+    public void NhapThongTin() {
+        System.out.println("Moi nhap thong tin ca nhan Quan Ly");
+    }
+
+    public void LayThongTin() {
+        System.out.println("Day la thong tin ca nhan Quan Ly");
+    }
+
+    public void LayTaiKhoan() {
+        System.out.println("Day la thong tin tai khoan Quan Ly");
+    }
+
+    //methods
+    public int DangNhapTaiKhoanQuanLy() throws IOException {
+        System.out.print("Moi nhap tai khoan Quan Ly ");
+        String sTaiKhoan = sc.nextLine();
+        System.out.print("Moi nhap mat khau Quan Ly ");
+        String sMatKhau = sc.nextLine();
+        File file = new File(FileNameQuanLy);
+        int iLogin = 1;
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(new FileReader(FileNameQuanLy));
+        String sline;
+        String fiQuanLy[] = new String[999];
+        while ((sline = reader.readLine()) != null) {
+            fiQuanLy = sline.split(",");
+            if (fiQuanLy[0].trim().equals(sTaiKhoan) && fiQuanLy[1].trim().equals(sMatKhau)) {
+                iLogin = 0;
+            } else if (!fiQuanLy[0].trim().equals(sTaiKhoan) && !fiQuanLy[1].trim().equals(sMatKhau)) {
+                return 1;
+            } else if (!fiQuanLy[0].trim().equals(sTaiKhoan) || !fiQuanLy[1].trim().equals(sMatKhau)) {
+                iLogin = 2;
+            }
+        }
+        return iLogin;
+    }
+
+    public void CapNhatThongTinQuanLy() throws IOException {
+        File fileTmp = new File(FileTmp);
+        try {
+            if (!fileTmp.exists()) {
+                fileTmp.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File fileTTQL = new File(FileThongTinQL);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(FileTmp));
+        BufferedReader reader = new BufferedReader(new FileReader(FileThongTinQL));
+        QuanLyFile ql = new QuanLyFile("", "", "QuanLy");
+        System.out.print("Moi nhap ID cua Quan Ly can cap nhat ");
+        String ID = sc.nextLine();
+        String sLine;
+        String fiQuanLy[] = new String[10];
+        boolean bFound = false;
+        ql.NhapThongTin();
+        while ((sLine = reader.readLine()) != null) {
+            fiQuanLy = sLine.split(",");
+            if (fiQuanLy[0].equals(ID)) {
+                ql.setHoTen(sHoTenQuanLy);
+                ql.setDiaChi(sDiaChiQuanLy);
+                ql.setSoDienThoai(sSoDienThoaiQuanLy);
+                writer.write(ID + "," + ql.getHoTen() + "," + ql.getDiaChi() + "," + ql.getSoDienThoai() + ",1\n");
+                bFound = true;
+            } else {
+                writer.write(fiQuanLy[0] + "," + fiQuanLy[1] + "," + fiQuanLy[2] + "," + fiQuanLy[3] + ",1\n");
+            }
+        }
+        //}
+        writer.close();
+        reader.close();
+        if (bFound == true) {
+            if (!fileTTQL.delete()) {
+                System.out.println("Khong the xoa file!");
+            }
+            fileTmp.renameTo(new File(FileThongTinQL));
+            System.out.println("Da cap nhat thong tin Quan Ly ID = " + ID);
+        } else {
+            fileTmp.delete();
+            System.out.println("Khong tim thay Quan Ly mang ID = " + ID);
+        }
+    }
+
+    public void XemThongTinQuanLy() throws IOException {
+        File file = new File(FileThongTinQL);
+        String fiQuanLy[] = new String[10]; //dung de quan ly thong tin nhan vien
+        String sLine;
+        BufferedReader reader = new BufferedReader(new FileReader(FileThongTinQL));
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        QuanLyFile ql = new QuanLyFile("", "", "");
+        ql.LayThongTin();
+        boolean bFound = false;
+        System.out.printf("%-12s%-25s%-20s%-15s\n", "Ma QL", "Ho & ten", "Dia chi", "So dien thoai");
+
+        while ((sLine = reader.readLine()) != null) {
+            fiQuanLy = sLine.split(",");
+            if (fiQuanLy[4].equals("1")) {
+                bFound = true;
+                System.out.printf("%-12s%-25s%-20s%-15s\n", fiQuanLy[0], fiQuanLy[1], fiQuanLy[2], fiQuanLy[3]);
+            } else {
+                continue;
+            }
+        }
+        if (bFound == false) {
+            System.out.println("Khong co thong tin Quan Ly");
+        }
+    }
+
+    public void XemTaiKhoanQuanLy() throws IOException {
+        File file = new File(FileNameQuanLy);
+        String fiQuanLy[] = new String[10]; //dung de quan ly thong tin nhan vien
+        String sLine;
+        BufferedReader reader = new BufferedReader(new FileReader(FileNameQuanLy));
+        QuanLyFile ql = new QuanLyFile("", "", "");
+        ql.LayTaiKhoan();
+        System.out.printf("%-20s%-30s%-16s\n", "Tai khoan", "Mat khau", "Vai Tro");
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        boolean bFound = false;
+        while ((sLine = reader.readLine()) != null) {
+            fiQuanLy = sLine.split(",");
+            if (fiQuanLy[3].equals("1")) {
+                bFound = true;
+                System.out.printf("%-20s%-30s%-16s\n", fiQuanLy[0], fiQuanLy[1], fiQuanLy[2]);
+            }
+        }
+        if (bFound == false) {
+            System.out.println("Khong co thong tin tai khoan Quan Ly");
+        }
+    }
+
+    public void XemSanPham() {
+        DanhSachBut dsb = new DanhSachBut();
+        DanhSachVo dsv = new DanhSachVo();
+        DanhSachSach dss = new DanhSachSach();
+        dsb.xuatDSBut();
+        dsv.xuatDSVo();
+        dss.xuatDSSach();
+    }
+
+}
