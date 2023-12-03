95f4ecbb6d5d22169a147e4bd03be8944520045a
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2023-12-03 14:09:15 +0700

    Update ThongTinChuongTrinh.java

--- project/VanPhongPham/src/main/java/com/ThongTinChuongTrinh/ThongTinChuongTrinh.java
+++ project/VanPhongPham/src/main/java/com/ThongTinChuongTrinh/ThongTinChuongTrinh.java
@@ -86,27 +86,31 @@ public class ThongTinChuongTrinh {
         Date startDate = calStart.getTime();
 
         Calendar calEnd = Calendar.getInstance();
-        calEnd.set(2023, Calendar.JANUARY, 1);
+        calEnd.set(2024, Calendar.DECEMBER, 25);
         Date endDate = calEnd.getTime();
 
         ThongTinChuongTrinh chuongTrinh = new ThongTinChuongTrinh(
                 "1.0.0",
                 "Chuong trinh Ban van phong pham",
                 "",
-                "1. Nguyen Dai Quoc\n"
-                + "   - Facebook: fb.com/wolfdzai\n"
+                  "1. Nguyen Dai Quoc\n"
+                + "   - Facebook: https://wwww.facebook.com/wolfdzai\n"
                 + "   - Email: luutrithon1996@gmail.com\n"
                 + "   - Phone: 0931454176\n"
                 + "2. Dinh Phuc Thinh\n"
-                + "   - Facebook: fb.com/dinfucthin\n"
+                + "   - Facebook: https://wwww.facebook.com/dinfucthin\n"
                 + "   - Email: zaikaman123@gmail.com\n"
                 + "   - Phone: 0931816175\n"
                 + "3. Tran Trung Viet\n"
-                + "   - Facebook: cutt.ly/qwTNnwZY\n"
-                + "4. Tran Dang Phat\n"
-                + "   - Facebook: cutt.ly/TwTNb3Ur\n"
+                + "   - Facebook: https://wwww.facebook.com/profile.php?id=100019759148684\n"
+                + "   - Email: tr.trungviet04@gmail.com\n"
+                + "   - Phone: 0937024425\n"
+                + "4. Tran Dang Phat\n" 
+                + "   - Facebook: https://www.facebook.com/profile.php?id=100022698711713\n"
+                + "   - Email: phattran280704@gmail.com\n"
+                + "   - Phone: 0779792132\n"
                 + "5. Nguyen Hoang Sang\n"
-                + "   - Facebook: cutt.ly/4wTNbGKf\n"
+                + "   - Facebook: https://www.facebook.com/profile.php?id=100041332085485\n"
                 + "   - Email: lesang01227982715@gmail.com\n"
                 + "   - Phone: 0776592967\n",
                 startDate,
@@ -114,7 +118,7 @@ public class ThongTinChuongTrinh {
         );
         Date check = new Date();
         System.out.println("Chuong trinh: " + chuongTrinh.sTenChuongTrinh);
-        System.out.println("Version: " + chuongTrinh.sMoTa);
+        System.out.println("Version: " + chuongTrinh.sPhienBan);
         System.out.println("Release Date: " + chuongTrinh.dtNgayBatDau);
         if (chuongTrinh.dtNgayKetThuc == check) {
             System.out.println("Stop Date: " + chuongTrinh.dtNgayKetThuc);
