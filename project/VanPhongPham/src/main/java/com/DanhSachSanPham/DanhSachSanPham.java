fc1e1d9cce0c1fb02bf08bc6231647f01b343c2a
zaikaman <91252891+zaikaman@users.noreply.github.com>
2023-12-03 15:25:03 +0700

    Update DanhSachSanPham.java

--- project/VanPhongPham/src/main/java/com/DanhSachSanPham/DanhSachSanPham.java
+++ project/VanPhongPham/src/main/java/com/DanhSachSanPham/DanhSachSanPham.java
@@ -1,4 +1,3 @@
-
 package com.DanhSachSanPham;
 
 import com.DanhSachBut.DanhSachBut;
@@ -7,13 +6,14 @@ import com.DanhSachVo.DanhSachVo;
 import java.util.Scanner;
 
 public class DanhSachSanPham {
+
     private DanhSachBut dsBut;
     private DanhSachSach dsSach;
     private DanhSachVo dsVo;
-    
+
     Scanner sc = new Scanner(System.in);
 
-    public void menu(){
+    public void menu() {
         System.out.println("0.Thoat");
         System.out.println("1.Nhap Danh Sach San Pham");
         System.out.println("2.Xuat Danh Sach San Pham");
@@ -21,51 +21,53 @@ public class DanhSachSanPham {
         System.out.println("4.Xoa San Pham");
         System.out.println("5.Sua San Pham");
     }
-    
-    public void menu1(){
+
+    public void menu1() {
         System.out.println("0.Thoat");
         System.out.println("1.But");
         System.out.println("2.Sach");
         System.out.print("3.Vo");
     }
-    
-    public void timKiemSanPham(String keyword) {
-    System.out.println("Ket qua tim kiem cho \"" + keyword + "\":");
-    
-    System.out.println("\nBut:");
-    dsBut.timKiemBut(keyword);
-    
-    System.out.println("\nSach:");
-    dsSach.timKiemSach(keyword);
-    
-    System.out.println("\nVo:");
-    dsVo.timKiemVo(keyword);
-}
 
-    public void thucHien(){
+    public void timKiemSanPham(String keyword) {
+        System.out.println("Ket qua tim kiem cho \"" + keyword + "\":");
+
+        System.out.println("\nBut:");
+        dsBut.timKiemBut(keyword);
+
+        System.out.println("\nSach:");
+        dsSach.timKiemSach(keyword);
+
+        System.out.println("\nVo:");
+        dsVo.timKiemVo(keyword);
+    }
+
+    public void thucHien() {
         dsBut = new DanhSachBut();
         dsSach = new DanhSachSach();
         dsVo = new DanhSachVo();
-        while(true){
+        while (true) {
             menu();
             int choose;
             System.out.print("Moi ban nhap lua chon: ");
             choose = Integer.parseInt(sc.nextLine());
-            if(choose == 0){
+            if (choose == 0) {
                 break;
             }
-            switch(choose){
-                case 1: 
-                    while(true){
+            switch (choose) {
+                case 1:
+                    while (true) {
                         menu1();
                         System.out.println();
                         int choose1;
                         System.out.print("Moi ban nhap lua chon: ");
                         choose1 = Integer.parseInt(sc.nextLine());
-                        if(choose1 == 0) break;
-                        switch(choose1){
+                        if (choose1 == 0) {
+                            break;
+                        }
+                        switch (choose1) {
                             case 1:
-                                dsBut.nhapDSBut(); 
+                                dsBut.nhapDSBut();
                                 dsBut.ghiFileDSBut();
                                 break;
                             case 2:
@@ -80,75 +82,95 @@ public class DanhSachSanPham {
                     }
                     break;
                 case 2:
-                    while(true){
+                    while (true) {
                         menu1();
                         int choose2;
                         System.out.println();
                         System.out.print("Moi ban nhap lua chon: ");
                         choose2 = Integer.parseInt(sc.nextLine());
-                        if(choose2 == 0) break;
-                        switch(choose2){
+                        if (choose2 == 0) {
+                            break;
+                        }
+                        switch (choose2) {
                             case 1:
-                                dsBut.docFileDSBut(); break;
+                                dsBut.printButProducts();
+                                break;
                             case 2:
-                                dsSach.docFileDSSach(); break;
+                                dsSach.printSachProducts();
+                                break;
                             case 3:
-                                dsVo.docFileDSVo(); break;
-                                
+                                dsVo.printVoProducts();
+                                break;
+
                         }
                     }
                     break;
                 case 3:
-                    while(true){
+                    while (true) {
                         menu1();
                         int choose3;
                         System.out.println();
                         System.out.print("Moi ban nhap lua chon: ");
                         choose3 = Integer.parseInt(sc.nextLine());
-                        if(choose3 == 0) break;
-                        switch(choose3){
+                        if (choose3 == 0) {
+                            break;
+                        }
+                        switch (choose3) {
                             case 1:
-                                dsBut.addBut(); break;
+                                dsBut.addBut();
+                                break;
                             case 2:
-                                dsSach.addSach(); break;
+                                dsSach.addSach();
+                                break;
                             case 3:
-                                dsVo.addVo(); break;
+                                dsVo.addVo();
+                                break;
                         }
                     }
                     break;
                 case 4:
-                    while(true){
+                    while (true) {
                         menu1();
                         int choose4;
                         System.out.println();
                         System.out.print("Moi ban nhap lua chon: ");
                         choose4 = Integer.parseInt(sc.nextLine());
-                        if(choose4 == 0) break;
-                        switch(choose4){
+                        if (choose4 == 0) {
+                            break;
+                        }
+                        switch (choose4) {
                             case 1:
-                                dsBut.deleteBut(); break;
+                                dsBut.deleteBut();
+                                break;
                             case 2:
-                                dsSach.deleteSach(); break;
+                                dsSach.deleteSach();
+                                break;
                             case 3:
-                                dsVo.deleteVo(); break;
+                                dsVo.deleteVo();
+                                break;
                         }
                     }
                     break;
                 case 5:
-                    while(true){
+                    while (true) {
                         menu1();
                         int choose5;
                         System.out.println();
                         System.out.print("Moi ban nhap lua chon: ");
                         choose5 = Integer.parseInt(sc.nextLine());
-                        if(choose5 == 0) break;
-                        switch(choose5){
+                        if (choose5 == 0) {
+                            break;
+                        }
+                        switch (choose5) {
                             case 1:
-                                dsBut.updateBut(); break;
+                                dsBut.updateBut();
+                                break;
                             case 2:
-                                dsSach.updateSach(); break;
+                                dsSach.updateSach();
+                                break;
                             case 3:
-                                dsVo.updateSach(); break;
+                                dsVo.updateSach();
+                                break;
                         }
                     }
                     break;
